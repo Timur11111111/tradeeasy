7c2b8dbca34fd76752943f06a86f62193d41e93d
Timur <newmew@tutanota.com>
2024-08-20 21:42:52 +0500

    cart

--- TradeEasy/urls.py
+++ TradeEasy/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
@@ -29,3 +30,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    
