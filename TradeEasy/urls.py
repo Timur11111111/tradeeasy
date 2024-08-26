06c0432d3f10a0eed834e97540a0a2aa7e68b6ce
Timur <newmew@tutanota.com>
2024-08-26 23:17:02 +0500

    469

--- TradeEasy/urls.py
+++ TradeEasy/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('payment/', include('payment.urls', namespace='payment')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
